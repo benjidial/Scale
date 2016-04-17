aa910efde0b5273fb9a471c62a171347420c4a6e
Benji <benji3.141@gmail.com>
2016-04-17 15:53:16 -0400

    Added more scales
    *changed internal representation from semitones to cents
    +whole tone
    +phrygian
    +pentatonic
    +minor pentatonic
    +locrian
    +mixolydian
    +aeolian alias for minor
    +melodic minor
    +harmonic minor
    +ionian alias for major
    +lydian
    +dorian
    +blues

--- Scale/Program.cs
+++ Scale/Program.cs
@@ -43,15 +43,49 @@ static class Program
             Console.WriteLine(rm.GetString("CommandLineFormat"));
             return -3;
         }
-        double step = Math.Pow(2, 1d / 12d);
-        int[] keys;
+        int[] cents;
         switch (args[0].ToLower(System.Globalization.CultureInfo.CurrentCulture))
         {
-            case "minor":
-                keys = new int[ ] { 0, 2, 3, 5, 7, 8, 10, 12, 10, 8, 7, 5, 3, 2, 0 };
+            case "whole tone":
+                cents = new int[ ] { 0, 200, 400, 600, 800, 1000, 1200, 1000, 800, 600, 400, 200, 0 };
                 break;
+            case "phrygian":
+                cents = new int[ ] { 0, 100, 300, 500, 700, 800, 1000, 1200, 1000, 800, 700, 500, 300, 100, 0 };
+                break;
+            case "pentatonic":
+                cents = new int[ ] { 0, 200, 400, 700, 900, 1200, 900, 700, 400, 200, 0 };
+                break;
+            case "minor pentatonic":
+                cents = new int[ ] { 0, 300, 500, 700, 1000, 1200, 1000, 700, 500, 300, 0 };
+                break;
+            case "locrian":
+                cents = new int[ ] { 0, 100, 300, 500, 600, 800, 1000, 1200, 1000, 800, 600, 500, 300, 100, 0 };
+                break;
+            case "mixolydian":
+                cents = new int[ ] { 0, 200, 400, 500, 700, 900, 1000, 1200, 1000, 900, 700, 500, 400, 200, 0 };
+                break;
+            case "aeolian":
+            case "minor":
+                cents = new int[ ] { 0, 200, 300, 500, 700, 800, 1000, 1200, 1000, 800, 700, 500, 300, 200, 0 };
+                break;
+            case "melodic minor":
+                cents = new int[ ] { 0, 200, 300, 500, 700, 900, 1100, 1200, 1000, 800, 700, 500, 300, 200, 0 };
+                break;
+            case "harmonic minor":
+                cents = new int[ ] { 0, 200, 300, 500, 700, 800, 1100, 1200, 1100, 800, 700, 500, 300, 200, 0 };
+                break;
+            case "ionian":
             case "major":
-                keys = new int[ ] { 0, 2, 4, 5, 7, 9, 11, 12, 11, 9, 7, 5, 4, 2, 0 };
+                cents = new int[ ] { 0, 200, 400, 500, 700, 900, 1100, 1200, 1100, 900, 700, 500, 400, 200, 0 };
+                break;
+            case "lydian":
+                cents = new int[ ] { 0, 200, 400, 600, 700, 900, 1100, 1200, 1100, 900, 700, 600, 400, 200, 0 };
+                break;
+            case "dorian":
+                cents = new int[ ] { 0, 200, 300, 500, 700, 900, 1000, 1200, 1000, 900, 700, 500, 300, 200, 0 };
+                break;
+            case "blues":
+                cents = new int[ ] { 0, 300, 500, 600, 700, 1000, 1200, 1000, 700, 600, 500, 300, 0 };
                 break;
             default:
                 Console.WriteLine(rm.GetString("IncorrectScaleType"), args[0]);
@@ -59,9 +93,9 @@ static class Program
                 Console.WriteLine(rm.GetString("CommandLineFormat"));
                 return -4;
         }
-        foreach (int key in keys)
+        foreach (int key in cents)
         {
-            Console.Beep((int)(start * Math.Pow(step, key)), 750);
+            Console.Beep((int)(start * Math.Pow(2, (double)key / 1200)), 750);
         }
         return -1;
     }
